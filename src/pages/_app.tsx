2a58593da9df347d9e31a28b3dff610b081cb6fd
Akshay Shejwal <akshay.shejwal@neosoftmail.com>
2024-05-22 11:13:38 +0530

    Added favicon and title and description

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -143,12 +143,9 @@ const App = (props: ExtendedAppProps) => {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>{`${themeConfig.templateName} - Material Design React Admin Template`}</title>
-        <meta
-          name='description'
-          content={`${themeConfig.templateName} – Material Design React Admin Dashboard Template – is the most developer friendly & highly customizable Admin Dashboard Template based on MUI v5.`}
-        />
-        <meta name='keywords' content='Material Design, MUI, Admin Template, React Admin Template' />
+        <title>{`${themeConfig.templateName} - Ampersand`}</title>
+        <meta name='description' content={`${themeConfig.templateName} Ampersand`} />
+        <meta name='keywords' content='Ampersand' />
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
       <GlobalProvider>
